a6b3b673c5343a8c2f92425e4f72be1e67868b61
nharibabu <nharibabu@csc.com>
2017-11-17 10:51:51 +0000

    Saving first time

---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private int ctr = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ctr = ctr + 1;
+            label1.Text = ctr.ToString();
+        }
+    }
+}
