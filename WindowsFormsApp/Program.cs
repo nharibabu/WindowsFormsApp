bcda22e2f671fa9378fb6b35fee9c910b56f81de
nharibabu <nharibabu@csc.com>
2017-12-05 15:49:30 +0530

    Update Program.cs

--- WindowsFormsApp/Program.cs
+++ WindowsFormsApp/Program.cs
@@ -15,8 +15,8 @@ namespace WindowsFormsApp
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+           // Application.SetCompatibleTextRenderingDefault(false);
+           // Application.Run(new Form1());
         }
     }
 }
